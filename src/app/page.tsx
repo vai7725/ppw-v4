ba21684522db6785bbdcdb7176ee9d0d73ee355f
vai7725 <vaibhav.prajapat.7725@gmail.com>
2024-03-18 12:18:01 +0530

    daisyUI setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,5 @@ import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <div>
-      <Button>Hello</Button>
-    </div>
-  )
+  return <div></div>
 }
